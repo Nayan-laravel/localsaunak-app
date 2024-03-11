567fba4713103f50ce8e37205dcbe69d99f25afe
Nayan <nayan.rulingrowth@gmail.com>
2024-03-11 11:58:41 +0530

    AdminLogin with Userlogin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+//Admin Route
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+    Route::namespace('Auth')->middleware('guest:admin')->group(function(){
+        //login route
+        Route::get('login',[AuthenticatedSessionController::class, 'create'])->name('login'); 
+        Route::post('login',[AuthenticatedSessionController::class, 'store'])->name('adminlogin');  
+
+    }); 
+    Route::middleware('admin')->group(function(){
+        
+        Route::get('dashboard',[HomeController::class, 'index'])->name('dashboard');  
+        
+    }); 
+    Route::POST('logout',[AuthenticatedSessionController::class, 'destroy'])->name('logout');  
+
 });
